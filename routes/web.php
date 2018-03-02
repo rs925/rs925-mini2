61631a8d37ee09dfccba67d9f3dc10ccd24e25c1
Rahul Sharan <rs925@njit.edu>
2018-03-01 22:16:53 -0500

    files added

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index')->name('index');
+
+Route::get('/about', 'PagesController@about')->name('about');
+
+Route::get('/contact', 'PagesController@contact')->name('contact');
+Route::get('/signup', 'PagesController@signup')->name('signup');
+Route::get('/signin', 'PagesController@signin')->name('signin');
+Route::get('/viewmessage', 'PagesController@viewmessage')->name('viewmessage');
+Route::post('/contact', 'PagesController@store')->name('contact.store');
+Route::get('/thanks/{name}', 'PagesController@thanks')->name('thanks');
